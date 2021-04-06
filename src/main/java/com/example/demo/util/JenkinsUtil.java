22cbbfaf9ec24389540fbf311f204a25fae1007f
wuchao <n2s_LZ39>
2021-04-06 23:10:22 +0800

    判断job是否存在，存在直接构建，否则新建

--- src/main/java/com/example/demo/util/JenkinsUtil.java
+++ src/main/java/com/example/demo/util/JenkinsUtil.java
@@ -26,15 +26,15 @@ public class JenkinsUtil {
         String jenkinsVersion = jenkinsHttpClient.getJenkinsVersion();
         System.out.println("jenkinsVersion:" + jenkinsVersion);
         JenkinsServer jenkinsServer = new JenkinsServer(jenkinsHttpClient);
-        Map<String, Job> jobs = jenkinsServer.getJobs();
         //判断如果jobName不为空则更新Job，否则创建Job
-        if (jobs.get(jobName) != null){
-            jenkinsServer.updateJob(jobName,jobConfigXml,true);
-        }else {
+        if (jobName.isEmpty()){
             jenkinsServer.createJob(jobName,jobConfigXml,true);
+        }else {
+            jenkinsServer.updateJob(jobName,jobConfigXml,true);
         }
 //        jenkinsServer.updateJob(jobName,jobConfigXml,true);
         //todo:新建job无法build，需bugfix
+        Map<String, Job> jobs = jenkinsServer.getJobs();
         Job job = jobs.get(jobName);
         //jenkins参数化构建传参
         Map<String,String> parameter = new HashMap<>();
